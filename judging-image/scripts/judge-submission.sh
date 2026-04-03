65182a8b3db29a6d3b000029416118882449aaa0
Eriq Augustine <eriq.augustine@gmail.com>
2026-04-03 10:02:53 -0700

    Added submission execution to the judging script.

--- judging-image/scripts/judge-submission.sh
+++ judging-image/scripts/judge-submission.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 readonly INPUT_DIRNAME='input'
-readonly OUTOUT_DIR='output'
+readonly OUTPUT_DIRNAME='output'
 readonly ORACLE_DIRNAME='oracle'
 
 readonly SUBMISSION_BASENAME='solution'
 
-readonly ALLOWED_EXTENSIONS=".c .cc .py"
+readonly ALLOWED_EXTENSIONS=".py"
 
+readonly OUTPUT_DIFF_PATH='/tmp/qfcc-output.diff'
+
+# Check the structure of the base directory.
 function check_contents() {
-    local baseDir=$1
+    local base_dir=$1
 
     declare -A paths=(
-        ['Base']="${baseDir}"
-        ['Input']="${baseDir}/${INPUT_DIRNAME}"
-        ['Output']="${baseDir}/${OUTPUT_DIRNAME}"
-        ['Oracle Input']="${baseDir}/${ORACLE_DIRNAME}/${INPUT_DIRNAME}"
-        ['Oracle Output']="${baseDir}/${ORACLE_DIRNAME}/${OUTPUT_DIRNAME}"
+        ['Base']="${base_dir}"
+        ['Input']="${base_dir}/${INPUT_DIRNAME}"
+        ['Output']="${base_dir}/${OUTPUT_DIRNAME}"
+        ['Oracle Input']="${base_dir}/${ORACLE_DIRNAME}/${INPUT_DIRNAME}"
+        ['Oracle Output']="${base_dir}/${ORACLE_DIRNAME}/${OUTPUT_DIRNAME}"
     )
 
     for name in "${!paths[@]}" ; do
@@ -24,55 +27,128 @@ function check_contents() {
 
         if [[ ! -d "${path}" ]] ; then
             echo "ERROR: ${name} directory does not exist or is not a directory: '${path}'."
-            exit 2
+            exit 102
         fi
     done
 
-    local inputDir="${baseDir}/${INPUT_DIRNAME}"
+    local input_dir="${base_dir}/${INPUT_DIRNAME}"
 
-    local numInputFiles=$(ls -1q "${inputDir}" | wc -l)
-    if [[ ${numInputFiles} -ne 1 ]] ; then
-        echo "ERROR: Expected exactly one input file, found ${numInputFiles}."
-        exit 3
+    local num_input_files=$(ls -1q "${input_dir}" | wc -l)
+    if [[ ${num_input_files} -ne 1 ]] ; then
+        echo "ERROR: Expected exactly one input file, found ${num_input_files}."
+        exit 103
     fi
 
-    local submissionPath=$(realpath "${inputDir}/"*)
+    local submission_path=$(realpath "${input_dir}/"*)
 
-    if [[ ! -f ${submissionPath} ]] ; then
+    if [[ ! -f ${submission_path} ]] ; then
         echo "ERROR: Submission file does not exist, or is not a file."
-        exit 4
+        exit 104
     fi
 
-    local submissionBasename=$(basename "${submissionPath}" | sed 's/\(.*\)\(\.\w\+\)$/\1/')
-    local submissionExtension=$(basename "${submissionPath}" | sed 's/\(.*\)\(\.\w\+\)$/\2/')
+    local submission_basename=$(basename "${submission_path}" | sed 's/\(.*\)\(\.\w\+\)$/\1/')
+    local submission_extension=$(basename "${submission_path}" | sed 's/\(.*\)\(\.\w\+\)$/\2/')
 
-    if [[ "${SUBMISSION_BASENAME}" != "${submissionBasename}" ]] ; then
-        echo "ERROR: Submission does not have the correct basename ('${SUBMISSION_BASENAME}'), found: '${submissionBasename}'."
-        exit 5
+    if [[ "${SUBMISSION_BASENAME}" != "${submission_basename}" ]] ; then
+        echo "ERROR: Submission does not have the correct basename ('${SUBMISSION_BASENAME}'), found: '${submission_basename}'."
+        exit 105
     fi
 
-    if [[ "${ALLOWED_EXTENSIONS}" != *"${submissionExtension}"* ]] ; then
-        echo "ERROR: Submission does not have the correct extension, found: '${submissionExtension}'."
-        exit 6
+    if [[ "${ALLOWED_EXTENSIONS}" != *"${submission_extension}"* ]] ; then
+        echo "ERROR: Submission does not have the correct extension, found: '${submission_extension}'."
+        exit 106
+    fi
+
+    for oracle_input_path in "${base_dir}/${ORACLE_DIRNAME}/${INPUT_DIRNAME}/"* ; do
+        local test_name=$(basename "${oracle_input_path}" | sed 's/\(.*\)\(\.\w\+\)$/\1/')
+
+        if [[ ! -f "${oracle_input_path}" ]] ; then
+            echo "ERROR: Input oracle for test case '${test_name}' is not a file: '${oracle_input_path}'."
+            exit 107
+        fi
+
+        local oracle_output_path="${base_dir}/${ORACLE_DIRNAME}/${OUTPUT_DIRNAME}/${test_name}.txt"
+        if [[ ! -f "${oracle_output_path}" ]] ; then
+            echo "ERROR: Output oracle for test case '${test_name}' does not exist or is not a file: '${oracle_output_path}'."
+            exit 108
+        fi
+    done
+}
+
+# Check against all test cases.
+function run_test_cases_and_exit() {
+    local base_dir=$1
+
+    local error_count=0
+
+    local submission_path=$(realpath "${base_dir}/${INPUT_DIRNAME}/"*)
+
+    for oracle_input_path in "${base_dir}/${ORACLE_DIRNAME}/${INPUT_DIRNAME}/"* ; do
+        local test_name=$(basename "${oracle_input_path}" | sed 's/\(.*\)\(\.\w\+\)$/\1/')
+        local oracle_output_path="${base_dir}/${ORACLE_DIRNAME}/${OUTPUT_DIRNAME}/${test_name}.txt"
+
+        local output_path="${base_dir}/${OUTPUT_DIRNAME}/${test_name}.txt"
+        local compile_output_path="${base_dir}/${OUTPUT_DIRNAME}/${test_name}_compile.txt"
+        local diff_out_path="${base_dir}/${OUTPUT_DIRNAME}/${test_name}.diff"
+
+        echo "Running test case: '${test_name}'."
+        run_submission "${submission_path}" "${oracle_input_path}" "${output_path}" "${compile_output_path}"
+
+        diff --side-by-side --width 150 --expand-tabs "${oracle_output_path}" "${output_path}" > "${diff_out_path}"
+        if [[ $? -ne 0 ]] ; then
+            ((error_count++))
+
+            diff --unified --label 'expected' --label 'actual' "${oracle_output_path}" "${output_path}" > "${OUTPUT_DIFF_PATH}"
+
+            echo "Test case '${test_name}' failed."
+            echo '------ unified diff ------'
+            cat "${OUTPUT_DIFF_PATH}"
+            echo '------------------'
+        else
+            echo "Test case '${test_name}' passed."
+        fi
+
+        echo ''
+    done
+
+    if [[ ${error_count} -gt 0 ]] ; then
+        echo "Failed ${error_count} test cases."
+    else
+        echo "Passed all test cases!"
+    fi
+
+    exit ${error_count}
+}
+
+function run_submission() {
+    local submission_path=$1
+    local input_path=$2
+    local output_path=$3
+    local compile_output_path=$4
+
+    local submission_extension=$(basename "${submission_path}" | sed 's/\(.*\)\(\.\w\+\)$/\2/')
+
+    if [[ "${submission_extension}" == '.py' ]] ; then
+        python3 "${submission_path}" < "${input_path}" &> "${output_path}"
+    else
+        echo "Unknown submission extension: '${submission_extension}'."
+        exit 109
     fi
 }
 
 function main() {
     if [[ $# -ne 1 ]]; then
         echo "USAGE: $0 <base dir>"
-        exit 1
+        exit 101
     fi
 
     trap exit SIGINT
-    set -e
 
-    local baseDir=$1
+    local base_dir=$1
 
-    check_contents "${baseDir}"
+    check_contents "${base_dir}"
 
-    # TODO
-
-    return 0
+    run_test_cases_and_exit "${base_dir}"
 }
 
 [[ "${BASH_SOURCE[0]}" == "${0}" ]] && main "$@"
